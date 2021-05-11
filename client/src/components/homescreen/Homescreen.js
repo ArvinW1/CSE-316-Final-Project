55069ca502cf330a50c29fca6a6da4d1f7caa9b0
Arvin <arvinwang2001@gmail.com>
2021-05-11 16:24:19 -0400

    Removed unneeded code

--- client/src/components/homescreen/Homescreen.js
+++ client/src/components/homescreen/Homescreen.js
@@ -26,20 +26,6 @@ import WCContent from 'wt-frontend/build/components/wcard/WCContent';
 
 function Homescreen(props) {
 
-	const keyCombination = (e, callback) => {
-		if (e.key === 'z' && e.ctrlKey) {
-			if (props.tps.hasTransactionToUndo()) {
-				tpsUndo();
-			}
-		}
-		else if (e.key === 'y' && e.ctrlKey) {
-			if (props.tps.hasTransactionToRedo()) {
-				tpsRedo();
-			}
-		}
-	}
-	document.onkeydown = keyCombination;
-
 	const auth = props.user === null ? false : true;
 	let todolists = [];
 	let maps = [];
@@ -53,7 +39,6 @@ function Homescreen(props) {
 	const [canUndo, setCanUndo] = useState(props.tps.hasTransactionToUndo());
 	const [canRedo, setCanRedo] = useState(props.tps.hasTransactionToRedo());
 
-	//const { loading, error, data, refetch } = useQuery(GET_DB_TODOS);
 	const { loading, error, data, refetch } = useQuery(GET_DB_MAP)
 
 	if (loading) { console.log(loading, 'loading'); }
@@ -66,13 +51,6 @@ function Homescreen(props) {
 			}
 		}
 		console.log(maps)
-		// if a list is selected, shift it to front of todolists
-		// if(activeList._id) {
-		// 	let selectedListIndex = todolists.findIndex(entry => entry._id === activeList._id);
-		// 	let removed = todolists.splice(selectedListIndex, 1);
-		// 	todolists.unshift(removed[0]);
-		// }
-		// create data for sidebar links
 		for (let map of maps) {
 			if (map) {
 				SidebarData.push({ _id: map._id, name: map.name, subregions: map.subregions});
@@ -105,14 +83,6 @@ function Homescreen(props) {
 		//onCompleted: () => reloadList()
 	}
 
-	const [ReorderTodoItems] = useMutation(mutations.REORDER_ITEMS, mutationOptions);
-	const [sortTodoItems] = useMutation(mutations.SORT_ITEMS, mutationOptions);
-	const [UpdateTodoItemField] = useMutation(mutations.UPDATE_ITEM_FIELD, mutationOptions);
-	const [UpdateTodolistField] = useMutation(mutations.UPDATE_TODOLIST_FIELD, mutationOptions);
-	const [DeleteTodoItem] = useMutation(mutations.DELETE_ITEM, mutationOptions);
-	const [AddTodoItem] = useMutation(mutations.ADD_ITEM, mutationOptions);
-	const [AddTodolist] = useMutation(mutations.ADD_TODOLIST);
-	const [DeleteTodolist] = useMutation(mutations.DELETE_TODOLIST);
 	const [AddMapList] = useMutation(mutations.ADD_MAPLIST);
 	const [DeleteMap] = useMutation(mutations.DELETE_MAP);
 	const [UpdateMapField] = useMutation(mutations.UPDATE_MAP_FIELD, mutationOptions);
@@ -134,75 +104,6 @@ function Homescreen(props) {
 		}
 	}
 
-	// const addItem = async () => {
-	// 	let list = activeList;
-	// 	const items = list.items;
-	// 	const newItem = { // <redirect to: /home/event.target._id/>
-	// 		_id: '',// /home/:_id
-	// 		description: 'No Description',
-	// 		due_date: 'No Date',
-	// 		assigned_to: 'No One',
-	// 		completed: false
-	// 	};
-	// 	let opcode = 1;
-	// 	let itemID = newItem._id;
-	// 	let listID = activeList._id;
-	// 	let transaction = new UpdateListItems_Transaction(listID, itemID, newItem, opcode, AddTodoItem, DeleteTodoItem);
-	// 	props.tps.addTransaction(transaction);
-	// 	tpsRedo();
-	// };
-
-	// const deleteItem = async (item, index) => {
-	// 	let listID = activeList._id;
-	// 	let itemID = item._id;
-	// 	let opcode = 0;
-	// 	let itemToDelete = {
-	// 		_id: item._id,
-	// 		description: item.description,
-	// 		due_date: item.due_date,
-	// 		assigned_to: item.assigned_to,
-	// 		completed: item.completed
-	// 	}
-	// 	let transaction = new UpdateListItems_Transaction(listID, itemID, itemToDelete, opcode, AddTodoItem, DeleteTodoItem, index);
-	// 	props.tps.addTransaction(transaction);
-	// 	tpsRedo();
-
-	// };
-
-	// const editItem = async (itemID, field, value, prev) => {
-	// 	let flag = 0;
-	// 	if (field === 'completed') flag = 1;
-	// 	let listID = activeList._id;
-	// 	let transaction = new EditItem_Transaction(listID, itemID, field, prev, value, flag, UpdateTodoItemField);
-	// 	props.tps.addTransaction(transaction);
-	// 	tpsRedo();
-
-	// };
-
-	const reorderItem = async (itemID, dir) => {
-		let listID = activeList._id;
-		let transaction = new ReorderItems_Transaction(listID, itemID, dir, ReorderTodoItems);
-		props.tps.addTransaction(transaction);
-		tpsRedo();
-
-	};
-
-	// const createNewList = async () => {
-	// 	let list = {
-	// 		_id: '',
-	// 		name: 'Untitled',
-	// 		owner: props.user._id,
-	// 		items: [],
-	// 		sortRule: 'task',
-	// 		sortDirection: 1
-	// 	}
-	// 	const { data } = await AddTodolist({ variables: { todolist: list }, refetchQueries: [{ query: GET_DB_TODOS }] });
-	// 	if(data) {
-	// 		loadTodoList(data.addTodolist);
-	// 	} 
-
-	// };
-
 	const createNewMap = async () => {
 		let map = {
 			_id: '',
@@ -221,11 +122,6 @@ function Homescreen(props) {
 			loadTodoList(data.addMaplist);
 		}
 	}
-	
-	// const deleteList = async (_id) => {
-	// 	DeleteTodolist({ variables: { _id: _id }, refetchQueries: [{ query: GET_DB_MAP }] });
-	// 	loadTodoList({});
-	// };
 
 	const deleteMap = async (_id) => {
 		DeleteMap({variables: {_id: _id}, refetchQueries: [{query: GET_DB_MAP}] });
@@ -234,16 +130,9 @@ function Homescreen(props) {
 
 	const updateListField = async (_id, field, value, prev) => {
 		const { data } = await UpdateMapField({ variables: { _id: _id, field: field, value: value }, refetchQueries: [{ query: GET_DB_MAP }] })
-		// let transaction = new UpdateListField_Transaction(_id, field, prev, value, UpdateTodolistField);
-		// props.tps.addTransaction(transaction);
-		// tpsRedo();
 
 	};
 
-	const handleSetActive = (_id) => {
-		const selectedList = todolists.find(todo => todo._id === _id);
-		loadTodoList(selectedList);
-	};
 
 	const setShowLogin = () => {
 		toggleShowDelete(false);
@@ -274,20 +163,6 @@ function Homescreen(props) {
 		toggleShowUpdate(!showUpdate)
 	}
 
-	const sort = (criteria) => {
-		let prevSortRule = sortRule;
-		setSortRule(criteria);
-		let transaction = new SortItems_Transaction(activeList._id, criteria, prevSortRule, sortTodoItems);
-		console.log(transaction)
-		props.tps.addTransaction(transaction);
-		tpsRedo();
-
-	}
-
-	const changePage = () => {
-		props.history.push("/anything")
-	}
-
 	return (
 		<WLayout wLayout="header">
 			<WLHeader>
@@ -308,39 +183,6 @@ function Homescreen(props) {
 				</WNavbar>
 			</WLHeader>
 
-			{/* <WLSide side="left">
-				<WSidebar>
-					{
-						activeList ? 
-							<SidebarContents
-								listIDs={SidebarData} 				activeid={activeList._id} auth={auth}
-								handleSetActive={handleSetActive} 	createNewList={createNewList}
-								updateListField={updateListField} 	key={activeList._id}
-							/>
-							:
-							<></>
-					}
-				</WSidebar>
-			</WLSide> */}
-			{/* <WLMain>
-				{
-					activeList ? 
-					
-							<div className="container-secondary">
-								<MainContents
-									addItem={addItem} 				deleteItem={deleteItem}
-									editItem={editItem} 			reorderItem={reorderItem}
-									setShowDelete={setShowDelete} 	undo={tpsUndo} redo={tpsRedo}
-									activeList={activeList} 		setActiveList={loadTodoList}
-									canUndo={canUndo} 				canRedo={canRedo}
-									sort={sort}
-								/>
-							</div>
-						:
-							<div className="container-secondary" />
-				}
-
-			</WLMain> */}
 			{
 				!auth && <WCard wLayout="content-footer" className="center"><WCContent className = {"homeMap"}><img src={HomeWorld} alt="HomeWorld" className = {"preLogGlobal"}/> </WCContent>
 					<WCFooter className="centerWord">WELCOME TO THE WORLD DATA MAPPER</WCFooter>
