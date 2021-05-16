ae49011c355fca1bd1d8a5facd770962172ecfdf
Arvin <arvinwang2001@gmail.com>
2021-05-15 20:58:21 -0400

    Implemented Error message for no named map

--- client/src/components/modals/Name.js
+++ client/src/components/modals/Name.js
@@ -3,27 +3,42 @@ import { WMHeader, WMMain, WModal, WInput, WButton, WMFooter } from 'wt-frontend
 
 const Name = (props) => {
 
-    const [input, setInput] = useState({});
+    const [showErr, displayErrorMsg] = useState(false);
+    const errorMsg = "Please enter a map name";
+    const [input, setInput] = useState('');
 
-    const handleCreate = () =>{
-        props.createNewMap(input)
-        props.setShowMapCreate(false)
+    const handleCreate = () => {
+        console.log(input)
+        if (input === '') {
+            displayErrorMsg(true);
+            return;
+        } else {
+            props.createNewMap(input)
+            props.setShowMapCreate(false)
+        }
     }
 
     return (
-        <WModal className="create-modal" cover="true" visible ={props.showMapCreate}>
+        <WModal className="create-modal" cover="true" visible={props.showMapCreate}>
             <WMHeader className="modal-header">
                 Name Map
             </WMHeader>
             <WMMain>
                 <WInput className="modal-input" onBlur={(e) => setInput(e.target.value)} name='mapName' labelAnimation="up" barAnimation="solid" labelText="Map Name" wType="outlined" inputType='text' />
+
+                {
+                    showErr ? <div className='modal-error'>
+                        {errorMsg}
+                    </div>
+                        : <div className='modal-error'>&nbsp;</div>
+                }
             </WMMain>
-            
+
             <WMFooter>
-				<WButton className="modal-button" onClick={handleCreate} span clickAnimation="ripple-light" hoverAnimation="darken" shape="rounded" color="primary">
-					Submit
+                <WButton className="modal-button" onClick={handleCreate} span clickAnimation="ripple-light" hoverAnimation="darken" shape="rounded" color="primary">
+                    Submit
 				</WButton>
-			</WMFooter>
+            </WMFooter>
         </WModal>
     )
 }
